9e8364b320521117a33e474c343bb8733becc144
Bryan Ong <bryangeeong@gmail.com>
2021-01-25 12:53:10 -0500

    Skeleton commit

---
+++ src/main/java/com/grocerease/app/controller/InventoryController.java
@@ -0,0 +1,9 @@
+package com.grocerease.app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InventoryController {
+
+    //This is where I would write something if I knew how
+}
---
+++ src/main/java/com/grocerease/app/service/InventoryService.java
@@ -0,0 +1,9 @@
+package com.grocerease.app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryService {
+
+    //Service classes go here
+}
