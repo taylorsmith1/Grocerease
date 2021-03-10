5448cc835c93ce85fd03b8a63d67fd8b277f84f9
Bryan Ong <bryangeeong@gmail.com>
2021-03-10 00:18:08 -0500

    yeet

--- grocerease/src/constants/apiConstants.js
+++ grocerease/src/constants/apiConstants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
+export const API_BASE_URL = 'localhost:8080';
 export const ACCESS_TOKEN_NAME = 'login_access_token';
--- src/main/java/com/grocerease/app/controller/UserController.java
+++ src/main/java/com/grocerease/app/controller/UserController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
@@ -49,8 +46,20 @@ public class UserController {
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/v1/login")
+    @PostMapping("/v1/login")
     public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/v1/login")
+    public String getlogin(Model model, String error, String logout) {
         if (error != null)
             model.addAttribute("error", "Your username and password is invalid.");
 
